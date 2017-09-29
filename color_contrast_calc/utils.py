97add583efe1d7f4642d4da8010108321337a50f
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-09-29 20:33:49 +0900

    add utils.hex_to_rgb()

--- color_contrast_calc/utils.py
+++ color_contrast_calc/utils.py
@@ -1,4 +1,16 @@
 # Utiity functions that provide basic operations on colors given as color codes
 
+def hex_to_rgb(hex_code):
+    if hex_code.find('#') == 0:
+        hex_part = hex_code[1:]
+    else:
+        hex_part = hex_code
+
+    if len(hex_part) == 3:
+        return list(map(lambda c: int(c, 16) * 17, hex_part))
+    elif len(hex_part) == 6:
+        primaries = map(lambda i: hex_part[i:(i+2)], [0, 2, 4])
+        return list(map(lambda c: int(c, 16), primaries))
+
 def rgb_to_hex(rgb):
     return '#{:02x}{:02x}{:02x}'.format(*rgb)
