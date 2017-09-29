9b1a13897d5b6bb9df0ee1381971f312dc183d58
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-09-29 20:47:34 +0900

    refactoring of utils.hex_to_rgb(): extract a private function

--- color_contrast_calc/utils.py
+++ color_contrast_calc/utils.py
@@ -1,10 +1,7 @@
 # Utility functions that provide basic operations on colors given as color codes
 
 def hex_to_rgb(hex_code):
-    if hex_code.find('#') == 0:
-        hex_part = hex_code[1:]
-    else:
-        hex_part = hex_code
+    hex_part = __remove_head_sharp(hex_code)
 
     if len(hex_part) == 3:
         return list(map(lambda c: int(c, 16) * 17, hex_part))
@@ -14,3 +11,9 @@ def hex_to_rgb(hex_code):
 
 def rgb_to_hex(rgb):
     return '#{:02x}{:02x}{:02x}'.format(*rgb)
+
+def __remove_head_sharp(hex_code):
+    if hex_code.find('#') == 0:
+        return hex_code[1:]
+
+    return hex_code
