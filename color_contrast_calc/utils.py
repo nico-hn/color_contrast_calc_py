c750b75ce43a8e9d76a8c55d7f88442a7876f56a
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-09-29 20:36:24 +0900

    refactoring of utils.hex_to_rgb(): use a tupple in place of an array

--- color_contrast_calc/utils.py
+++ color_contrast_calc/utils.py
@@ -9,7 +9,7 @@ def hex_to_rgb(hex_code):
     if len(hex_part) == 3:
         return list(map(lambda c: int(c, 16) * 17, hex_part))
     elif len(hex_part) == 6:
-        primaries = map(lambda i: hex_part[i:(i+2)], [0, 2, 4])
+        primaries = map(lambda i: hex_part[i:(i+2)], (0, 2, 4))
         return list(map(lambda c: int(c, 16), primaries))
 
 def rgb_to_hex(rgb):
