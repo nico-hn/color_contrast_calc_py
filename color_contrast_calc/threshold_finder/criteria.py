c0ee7bd5bf37863359031d41c3e22af82c3a1617
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-03 23:44:27 +0900

    add threshold_finder.criteria

---
+++ color_contrast_calc/threshold_finder/criteria.py
@@ -0,0 +1,34 @@
+import math
+import color_contrast_calc.checker as checker
+
+class SearchDirection:
+    def __init__(self, level):
+        self.level = level
+        self.target_ratio = checker.level_to_ratio(level)
+
+class ToDarkerSide(SearchDirection):
+    def round(self, r):
+        return math.floor(r * 10) / 10.0
+
+    def increment_condition(self, contrast_ratio):
+        return contrast_ratio > self.target_ratio
+
+class ToBrighterSide(SearchDirection):
+    def round(self, r):
+        return math.ceil(r * 10) / 10.0
+
+    def increment_condition(self, contrast_ratio):
+        return self.target_ratio > contrast_ratio
+
+def threshold_criteria(level, fixed_color, other_color):
+    if should_scan_darker_side(fixed_color, other_color):
+        return ToDarkerSide(level)
+
+    return ToBrighterSide(level)
+
+def should_scan_darker_side(fixed_color, other_color):
+    higher_luminance = fixed_color.has_higher_luminance(other_color)
+    same_luminace = fixed_color.has_same_luminance(other_color)
+    is_light_color = fixed_color.is_light_color()
+
+    return higher_luminance or (is_light_color and same_luminace)
---
+++ tests/threshold_finder/test_criteria.py
@@ -0,0 +1,29 @@
+import unittest
+from color_contrast_calc.threshold_finder.criteria import threshold_criteria
+from color_contrast_calc.color import Color
+
+class TestCriteria(unittest.TestCase):
+    def setup(self):
+        pass
+
+    def test_criteria(self):
+        target = 'AA'
+        orange = Color.from_name('orange')
+        yellow = Color.from_name('yellow')
+        darkgreen = Color.from_name('darkgreen')
+
+        direction = threshold_criteria(target, orange, yellow)
+        self.assertTrue(direction.increment_condition(4.25))
+        self.assertEqual(direction.round(4.25), 4.3)
+
+        direction = threshold_criteria(target, yellow, orange)
+        self.assertFalse(direction.increment_condition(4.25))
+        self.assertEqual(direction.round(4.25), 4.2)
+
+        direction = threshold_criteria(target, yellow, yellow)
+        self.assertFalse(direction.increment_condition(4.25))
+        self.assertEqual(direction.round(4.25), 4.2)
+
+        direction = threshold_criteria(target, darkgreen, darkgreen)
+        self.assertTrue(direction.increment_condition(4.25))
+        self.assertEqual(direction.round(4.25), 4.3)
