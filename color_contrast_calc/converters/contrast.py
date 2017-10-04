b4fb29ab0d47e284ab801b697217ce7009783079
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-04 16:32:57 +0900

    change for coding style consistency in converters.contrast

--- color_contrast_calc/converters/contrast.py
+++ color_contrast_calc/converters/contrast.py
@@ -3,6 +3,6 @@
 
 from . import rgb_clamp
 
-def calc_rgb(rgb, ratio = 100):
+def calc_rgb(rgb, ratio=100):
     r = float(ratio)
     return rgb_clamp((c * r + 255 * (50 - r / 2)) / 100 for c in rgb)
