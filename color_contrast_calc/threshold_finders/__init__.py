a313c708c51e265d86bfb5b657be418eee0414ed
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-04 16:26:23 +0900

    cosmetic changes in threshold_finders

--- color_contrast_calc/threshold_finders/__init__.py
+++ color_contrast_calc/threshold_finders/__init__.py
@@ -1,9 +1,9 @@
-def binary_search_width(init_width, min):
+def binary_search_width(init_width, min_width):
     i = 1
     init_width = float(init_width)
     d = init_width / pow(2, i)
 
-    while d > min:
+    while d > min_width:
         yield d
-        i = i + 1
+        i += 1
         d = init_width / pow(2, i)
