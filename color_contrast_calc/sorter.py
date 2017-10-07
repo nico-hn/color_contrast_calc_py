2d96ef56e54cd7388ba681bdc9ee15864921a9a5
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-07 22:53:36 +0900

    add sorter.color_component_pos()

--- color_contrast_calc/sorter.py
+++ color_contrast_calc/sorter.py
@@ -2,6 +2,11 @@ import operator
 import re
 
 _HSL_RE = re.compile(r'[hsl]{3}', re.IGNORECASE)
+_RGB_COMPONENTS = 'rgb'
+_HSL_COMPONENTS = 'hsl'
 
 def is_hsl_order(color_order):
     return _HSL_RE.match(color_order) is not None
+
+def color_component_pos(color_order, ordered_components):
+    return tuple(ordered_components.find(c) for c in color_order.lower())
--- tests/test_sorter.py
+++ tests/test_sorter.py
@@ -12,3 +12,19 @@ class TestLightness(unittest.TestCase):
         self.assertTrue(sorter.is_hsl_order('lHs'))
         self.assertFalse(sorter.is_hsl_order('rgb'))
         self.assertFalse(sorter.is_hsl_order('bRg'))
+
+    def test_color_component_pos(self):
+        rgb = 'rgb'
+        hsl = 'hsl'
+
+        pos = sorter.color_component_pos('hsl', hsl)
+        self.assertEqual(pos, (0, 1, 2))
+
+        pos = sorter.color_component_pos('hLs', hsl)
+        self.assertEqual(pos, (0, 2, 1))
+
+        pos = sorter.color_component_pos('rgb', rgb)
+        self.assertEqual(pos, (0, 1, 2))
+
+        pos = sorter.color_component_pos('bgr', rgb)
+        self.assertEqual(pos, (2, 1, 0))
