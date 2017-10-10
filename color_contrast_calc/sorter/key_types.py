0a88db41b0d2cf61fde82ed3fe44f2f776b8b951
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-10 17:36:03 +0900

    add sorter.key_types

---
+++ color_contrast_calc/sorter/key_types.py
@@ -0,0 +1,15 @@
+from ..color import Color
+
+COLOR = 'COLOR'
+COMPONENTS = 'COMPONENTS'
+HEX = 'HEX'
+
+def guess(color, key_mapper=None):
+    key = color if key_mapper is None else key_mapper(color)
+
+    if isinstance(key, Color):
+        return COLOR
+    elif isinstance(key, str):
+        return HEX
+    else:
+        return COMPONENTS
---
+++ tests/sorter/test_key_types.py
@@ -0,0 +1,31 @@
+import unittest
+import operator
+from color_contrast_calc.color import Color
+from color_contrast_calc.sorter import sorter, key_types
+
+class TestKeyTypes(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_guess(self):
+        color = Color((255, 255, 0))
+        rgb = (255, 255, 0)
+        hsl = (60, 100, 50)
+        hex = '#ffff00'
+        colors = [color, rgb, hsl, hex]
+
+        self.assertEqual(key_types.guess(color), key_types.COLOR)
+        self.assertEqual(key_types.guess(colors, operator.itemgetter(0)),
+                         key_types.COLOR)
+
+        self.assertEqual(key_types.guess(rgb), key_types.COMPONENTS)
+        self.assertEqual(key_types.guess(colors, operator.itemgetter(1)),
+                         key_types.COMPONENTS)
+
+        self.assertEqual(key_types.guess(hsl), key_types.COMPONENTS)
+        self.assertEqual(key_types.guess(colors, operator.itemgetter(2)),
+                         key_types.COMPONENTS)
+
+        self.assertEqual(key_types.guess(hex), key_types.HEX)
+        self.assertEqual(key_types.guess(colors, operator.itemgetter(3)),
+                         key_types.HEX)
