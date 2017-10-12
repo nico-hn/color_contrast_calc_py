ec9a6bdb2e3c699302a4d8c7d167d5afd211d2aa
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-12 16:16:11 +0900

    cosmetic changes in color_contrast_calc.checker:
    top-level classes and functions should be surrounded with two blank lines

--- color_contrast_calc/checker.py
+++ color_contrast_calc/checker.py
@@ -16,12 +16,14 @@ _LEVEL_TO_RATIO = {
     1: 3
 }
 
+
 def relative_luminance(rgb):
     if isinstance(rgb, str):
         rgb = utils.hex_to_rgb(rgb)
 
     (r, g, b) = (_tristimulus_value(c) for c in rgb)
     return r * 0.2126 + g  * 0.7152 + b * 0.0722
+
 
 def _tristimulus_value(primary_color):
     base = 255
@@ -32,13 +34,16 @@ def _tristimulus_value(primary_color):
     else:
         return pow((s + 0.055) / 1.055, 2.4)
 
+
 def contrast_ratio(color1, color2):
     return luminance_to_contrast_ratio(relative_luminance(color1),
                                        relative_luminance(color2))
 
+
 def luminance_to_contrast_ratio(luminance1, luminance2):
     (l1, l2) = sorted((luminance1, luminance2), reverse=True)
     return (l1 + 0.05) / (l2 + 0.05)
+
 
 def ratio_to_level(ratio):
     if ratio >= 7:
@@ -49,6 +54,7 @@ def ratio_to_level(ratio):
         return WCAGLevel.A
 
     return '-'
+
 
 def level_to_ratio(level):
     if level in _LEVEL_TO_RATIO:
