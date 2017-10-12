600b955ed57a2994f1f15602d878deff5d11d391
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-12 16:11:39 +0900

    cosmetic changes in color_contrast_calc.utils:
    top-level functions should be surrounded with two blank lines

--- color_contrast_calc/utils.py
+++ color_contrast_calc/utils.py
@@ -6,6 +6,7 @@ import re
 
 _HEX_RE = re.compile(r'\A#?[0-9a-f]{3}([0-9a-f]{3})?\Z', re.IGNORECASE)
 
+
 def hex_to_rgb(hex_code):
     hex_part = _remove_head_sharp(hex_code)
 
@@ -15,8 +16,10 @@ def hex_to_rgb(hex_code):
         primaries = (hex_part[i:(i+2)] for i in (0, 2, 4))
         return tuple(int(c, 16) for c in primaries)
 
+
 def rgb_to_hex(rgb):
     return '#{:02x}{:02x}{:02x}'.format(*rgb)
+
 
 def normalize_hex(code, prefix=True):
     if len(code) < 6:
@@ -30,6 +33,7 @@ def normalize_hex(code, prefix=True):
 
     return '#' + lowered if prefix else lowered[1:]
 
+
 def hsl_to_rgb(hsl):
     h, s, l = (c / u for (c, u) in zip(hsl, (360.0, 100.0, 100.0)))
 
@@ -38,6 +42,7 @@ def hsl_to_rgb(hsl):
 
     adjusted_h = (h + 1 / 3.0, h, h - 1 / 3.0)
     return tuple(round(_hue_to_rgb(m1, m2, ah) * 255) for ah in adjusted_h)
+
 
 def _hue_to_rgb(m1, m2, h):
     if h < 0:
@@ -52,22 +57,27 @@ def _hue_to_rgb(m1, m2, h):
         return m1 + (m2 - m1) * (2 / 3.0 - h) * 6
     return m1
 
+
 def _remove_head_sharp(hex_code):
     if hex_code.startswith('#'):
         return hex_code[1:]
 
     return hex_code
 
+
 def hsl_to_hex(hsl):
     return rgb_to_hex(hsl_to_rgb(hsl))
+
 
 def rgb_to_hsl(rgb):
     return (_rgb_to_hue(rgb),
             _rgb_to_saturation(rgb) * 100,
             _rgb_to_lightness(rgb) * 100)
 
+
 def _rgb_to_lightness(rgb):
     return (max(rgb) + min(rgb)) / 510.0
+
 
 def _rgb_to_saturation(rgb):
     min_c = min(rgb)
@@ -83,6 +93,7 @@ def _rgb_to_saturation(rgb):
     else:
         return d / (510 - max_c - min_c)
 
+
 def _rgb_to_hue(rgb):
     min_c = min(rgb)
     max_c = max(rgb)
@@ -97,14 +108,18 @@ def _rgb_to_hue(rgb):
 
     return h + 360 if h < 0 else h
 
+
 def hex_to_hsl(hex_code):
     return rgb_to_hsl(hex_to_rgb(hex_code))
+
 
 def is_valid_rgb(rgb):
     return len(rgb) == 3 and all(_valid_rgb_component(c) for c in rgb)
 
+
 def _valid_rgb_component(component):
     return isinstance(component, int) and component >= 0 and component <= 255
+
 
 def is_valid_hsl(hsl):
     if len(hsl) != 3:
@@ -118,11 +133,14 @@ def is_valid_hsl(hsl):
 
     return True
 
+
 def is_valid_hex(hex_code):
     return _HEX_RE.match(hex_code) is not None
 
+
 def is_same_hex_color(hex1, hex2):
     return normalize_hex(hex1) == normalize_hex(hex2)
+
 
 def is_uppercase(string):
     return string == string.upper()
