f7532604e67f1ce2f73093c76b160e42983e04b5
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-13 15:04:33 +0900

    refactoring of threshold_finders.brightness.calc_upper_ratio_limit()

--- color_contrast_calc/threshold_finders/brightness.py
+++ color_contrast_calc/threshold_finders/brightness.py
@@ -75,5 +75,5 @@ def calc_upper_ratio_limit(color):
     if color.is_same_color(color.__class__.BLACK):
         return 100
 
-    darkest = min(filter(lambda c: c != 0, color.rgb))
+    darkest = min(c for c in color.rgb if c != 0)
     return math.ceil((255.0 / darkest) * 100)
