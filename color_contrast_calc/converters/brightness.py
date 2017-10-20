8a6a0be04d06605703498a957a3874df8e0c322f
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-20 15:43:19 +0900

    add docstring to converters.brightness.calc_rgb()

--- color_contrast_calc/converters/brightness.py
+++ color_contrast_calc/converters/brightness.py
@@ -5,5 +5,16 @@ from . import rgb_clamp
 
 
 def calc_rgb(rgb, ratio=100):
+    """Return brightness adjusted RGB value of passed color.
+
+    THe calculation is based on the definition found at
+    https://www.w3.org/TR/filter-effects/#funcdef-brightness
+    :param rgb: The Original RGB value before the adjustment.
+    :type rgb: (int, int, int)
+    :param ratio: Adjustment ratio in percentage [optional]
+    :type ratio: float
+    :return: Brightness adjusted RGB value
+    :rtype: (int, int, int)
+    """
     r = float(ratio)
     return rgb_clamp(c * r / 100 for c in rgb)
