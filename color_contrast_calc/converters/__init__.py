7d01f2176935d375dc4742b61694f87a6f544fcb
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-20 17:01:12 +0900

    add docstring to converters.rgb_clamp()

--- color_contrast_calc/converters/__init__.py
+++ color_contrast_calc/converters/__init__.py
@@ -2,6 +2,7 @@ import math
 
 
 def rgb_clamp(vals):
+    """Clamp the value of each RGB component to the range of 0 to 255"""
     return tuple(_adjusted_round(max(0, min(255, c))) for c in vals)
 
 
