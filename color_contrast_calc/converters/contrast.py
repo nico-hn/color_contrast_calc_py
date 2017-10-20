f29f6f62806171973bf474e94bf687b80d80c33e
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-20 15:49:22 +0900

    add docstring to converters.contrast.calc_rgb()

--- color_contrast_calc/converters/contrast.py
+++ color_contrast_calc/converters/contrast.py
@@ -5,5 +5,16 @@ from . import rgb_clamp
 
 
 def calc_rgb(rgb, ratio=100):
+    """Return contrast adjusted RGB value of passed color.
+
+    THe calculation is based on the definition found at
+    https://www.w3.org/TR/filter-effects/#funcdef-contrast
+    :param rgb: The Original RGB value before the adjustment.
+    :type rgb: (int, int, int)
+    :param ratio: Adjustment ratio in percentage [optional]
+    :type ratio: float
+    :return: Contrast adjusted RGB value
+    :rtype: (int, int, int)
+    """
     r = float(ratio)
     return rgb_clamp((c * r + 255 * (50 - r / 2)) / 100 for c in rgb)
