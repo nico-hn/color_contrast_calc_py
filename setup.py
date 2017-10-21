b3006ccb1101b5147d4c328597647ff93401fc1c
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-10-21 19:07:20 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,37 @@
 from setuptools import setup, find_packages
 
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='color-contrast-calc',
-    version='0.0.1',
-    description='Utility that supports you in choosing colors with sufficient contrast, WCAG 2.0 in mind',
+    version='0.1.0',
+    description='Utility that helps you choose colors with sufficient contrast, WCAG 2.0 in mind',
+    long_description=long_description,
     url='https://github.com/nico-hn/color_contrast_calc_py',
     author='HASHIMOTO, Naoki',
     author_email='hashimoto.naoki@gmail.com',
     license='MIT',
-    install_requires=[
-        'numpy>=1.3'
+    classifier=[
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
     ],
-    packages=find_packages(exclude=['tests*']),
+    python_requires='~=3.2',
+    install_requires=[
+        'numpy>=1.3',
+    ],
+    extras_requires={
+        'dev': ['pytest'],
+        'test': ['pylint']
+    },
+    packages=find_packages(exclude=['tests', 'docs']),
     package_data={
         'color_contrast_calc': ['color_keywords.json'],
     }
