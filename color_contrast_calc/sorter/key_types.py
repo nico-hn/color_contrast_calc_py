88790655e409f2063b1ccb4ac294ebcf1f40641c
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-11-24 23:49:51 +0900

    add docstring to sorter.key_types.guess()

--- color_contrast_calc/sorter/key_types.py
+++ color_contrast_calc/sorter/key_types.py
@@ -6,6 +6,17 @@ HEX = 'HEX'
 
 
 def guess(color, key_mapper=None):
+    """Return COLOR, COMPONENTS or HEX when a possible key value is passed.
+
+    :param color: Possible key value
+    :type color: Color or (int, int, int) or (float, float, float) or str
+    :param key_mapper: Function which retrieves a key value from ``color``,
+                       that means ``key_mapper(color)`` returns a key value.
+                       [optional]
+    :type key_mapper: function or None
+    :return: "COLOR", "COMPONENTS" or "HEX"
+    :rtype: str
+    """
     key = color if key_mapper is None else key_mapper(color)
 
     if isinstance(key, Color):
